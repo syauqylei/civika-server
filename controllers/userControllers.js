9751a2bb01130643212e52f9f7cfbd1f0a5d3aca
syauqy <syauqilenterano@gmail.com>
2021-05-28 13:56:34 +0700

    add put /user/:id

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,6 +4,7 @@ class UserControllers {
   static login(req, res, next) {}
   static getAll(req, res, next) {}
   static getById(req, res, next) {}
+  static editUser(req, res, next) {}
 }
 
 module.exports = UserControllers;
--- routers/userRouters.js
+++ routers/userRouters.js
@@ -3,5 +3,6 @@ const UserControllers = require("../controllers/userControllers.js");
 
 router.get("/users", UserControllers.getAll);
 router.get("/users/:id", UserControllers.getById);
+router.put("/users/:id", UserControllers.getById);
 router.post("/login", UserControllers.login);
 module.exports = router;
